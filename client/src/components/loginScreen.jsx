7e96029d89e2f6fcdd5a60bfd1f04e7bb03657e1
fmcfar200 <fmcfar200@caledonian.ac.uk>
2019-08-09 01:43:48 +0100

    Update loginScreen.jsx

--- client/src/components/loginScreen.jsx
+++ client/src/components/loginScreen.jsx
@@ -2,9 +2,23 @@ import React, { Component } from "react";
 import logo from "../logo.svg";
 import "../App.css";
 import "../styles/spotifyButton.css";
+require("dotenv").config();
 
+const DEV_SERVER = "http://localhost:8888";
+const PROD_SERVER = "https://my-fyauth.herokuapp.com/";
 class LoginScreen extends Component {
-  state = {};
+  state = {
+    authServerURL: PROD_SERVER
+  };
+
+  componentDidMount() {
+    if (process.env.NODE_ENV === "development") {
+      this.setState({
+        authServerURL: DEV_SERVER
+      });
+    }
+  }
+
   render() {
     return (
       <header className="App-header">
@@ -13,7 +27,7 @@ class LoginScreen extends Component {
           <div style={{ textAlign: "center" }}>
             {/* https://my-fyauth.herokuapp.com/ -production
               http://localhost:8888 - development */}
-            <a href="http://localhost:8888">
+            <a href={this.state.authServerURL}>
               <button className="Spotify-Button Spotify-Button-Trans">
                 Login To Spotify
               </button>
