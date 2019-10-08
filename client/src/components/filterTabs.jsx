7bc9988882819547532b5100ff5f7799872ff44e
fmcfar200 <fmcfar200@caledonian.ac.uk>
2019-10-08 23:29:47 +0100

    Refactored FilterTabs render code to filterRenderer.js
    filterRenderer.js to be used in both FilterTabs and FilterMobile

--- client/src/components/filterTabs.jsx
+++ client/src/components/filterTabs.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import InputRange from "react-input-range";
 import DropdownButton from "../common/dropdownButton";
-import { parseTrackKey } from "../utils/index";
+import {
+  renderKeyBox,
+  renderModeBox,
+  renderMinMaxBox
+} from "../utils/filterRenderer";
 import "react-input-range/lib/css/index.css";
 import "../styles/spotifyButton.css";
 import "../styles/filterTabs.css";
@@ -23,116 +26,12 @@ class FilterTabs extends Component {
     mode: null
   };
 
-  renderKeyBox() {
-    const { handleKeyRadioChange } = this.props;
-    const intKeyRange = Array.from({ length: 12 }, (x, i) => i); // range from 0-11 for keys
-
-    const radioBoxes = intKeyRange.map(key => {
-      return (
-        <div key={key}>
-          <input
-            type="radio"
-            name="key"
-            id={`keyChoice${key}`}
-            value={key}
-            onChange={handleKeyRadioChange}
-          />
-          <label htmlFor="keyChoice1">{parseTrackKey(key)}</label>
-        </div>
-      );
-    });
-
-    return (
-      <div className="tab-dropdown-container">
-        <label className="tab-dropdown-heading">Key</label>
-        <form className="radio">
-          <div className="key-grid">{radioBoxes}</div>
-        </form>
-      </div>
-    );
-  }
-
-  renderModeBox() {
-    const { handleModalityRadioChange } = this.props;
-
-    return (
-      <div className="tab-dropdown-container">
-        <label className="tab-dropdown-heading">Modality</label>
-        <form className="radio">
-          <div>
-            <div>
-              <input
-                type="radio"
-                name="modality"
-                id="modeChoice1"
-                value="0"
-                onChange={handleModalityRadioChange}
-              />
-              <label htmlFor="modeChoice1">Minor</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="modality"
-                id="modeChoice2"
-                value="1"
-                onChange={handleModalityRadioChange}
-              />
-              <label htmlFor="modeChoice2">Major</label>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
-
-  renderMinMaxBox(
-    heading,
-    minValue,
-    maxValue,
-    filterNameString,
-    step,
-    formatLabel
-  ) {
-    const { handleApplyFilterMinMax } = this.props;
-    return (
-      <div className="tab-dropdown-container">
-        <label className="tab-dropdown-heading">{heading}</label>
-        <div className="tab-input-container">
-          <InputRange
-            minValue={minValue}
-            maxValue={maxValue}
-            value={this.state[filterNameString]}
-            onChange={theValue => {
-              this.setState({ [filterNameString]: theValue });
-            }}
-            step={step}
-            formatLabel={value => `${value}${formatLabel}`}
-          />
-        </div>
-        <div className="tab-dropdown-button-container">
-          <button
-            className="button-add"
-            onClick={() =>
-              handleApplyFilterMinMax(
-                filterNameString,
-                this.state[filterNameString].min,
-                this.state[filterNameString].max
-              )
-            }
-          >
-            Add
-          </button>
-        </div>
-      </div>
-    );
-  }
-
   render() {
     const audioFeatureContent = (
       <div className="tab-dropdown-audioFeatures">
         <div>
-          {this.renderMinMaxBox(
+          {renderMinMaxBox(
+            this,
             "Acousticness",
             0.0,
             1.0,
@@ -142,7 +41,8 @@ class FilterTabs extends Component {
           )}
         </div>
         <div>
-          {this.renderMinMaxBox(
+          {renderMinMaxBox(
+            this,
             "Danceability",
             0.0,
             1.0,
@@ -152,10 +52,11 @@ class FilterTabs extends Component {
           )}
         </div>
         <div>
-          {this.renderMinMaxBox("Energy", 0.0, 1.0, "energy", 0.01, "")}
+          {renderMinMaxBox(this, "Energy", 0.0, 1.0, "energy", 0.01, "")}
         </div>
         <div>
-          {this.renderMinMaxBox(
+          {renderMinMaxBox(
+            this,
             "Instrumentalness",
             0.0,
             1.0,
@@ -165,10 +66,11 @@ class FilterTabs extends Component {
           )}
         </div>
         <div>
-          {this.renderMinMaxBox("Liveness", 0.0, 1.0, "liveness", 0.01, "")}
+          {renderMinMaxBox(this, "Liveness", 0.0, 1.0, "liveness", 0.01, "")}
         </div>
         <div>
-          {this.renderMinMaxBox(
+          {renderMinMaxBox(
+            this,
             "Speechiness",
             0.0,
             1.0,
@@ -178,7 +80,7 @@ class FilterTabs extends Component {
           )}
         </div>
         <div>
-          {this.renderMinMaxBox("Valence", 0.0, 1.0, "valence", 0.01, "")}
+          {renderMinMaxBox(this, "Valence", 0.0, 1.0, "valence", 0.01, "")}
         </div>
       </div>
     );
@@ -194,7 +96,8 @@ class FilterTabs extends Component {
         <DropdownButton
           buttonClass="btn tab-button"
           buttonLabel="Tempo"
-          bodyContent={this.renderMinMaxBox(
+          bodyContent={renderMinMaxBox(
+            this,
             "Tempo (BPM)",
             60,
             140,
@@ -206,12 +109,12 @@ class FilterTabs extends Component {
         <DropdownButton
           buttonClass="btn tab-button"
           buttonLabel="Key"
-          bodyContent={this.renderKeyBox()}
+          bodyContent={renderKeyBox(this.props)}
         />
         <DropdownButton
           buttonClass="btn tab-button"
           buttonLabel="Modality"
-          bodyContent={this.renderModeBox()}
+          bodyContent={renderModeBox(this.props)}
         />
 
         {/* <DropdownButton
@@ -223,7 +126,8 @@ class FilterTabs extends Component {
         <DropdownButton
           buttonClass="btn tab-button"
           buttonLabel="Popularity"
-          bodyContent={this.renderMinMaxBox(
+          bodyContent={renderMinMaxBox(
+            this,
             "Popularity",
             0,
             100,
---
+++ client/src/utils/filterRenderer.js
@@ -0,0 +1,109 @@
+import React from "react";
+import InputRange from "react-input-range";
+import { parseTrackKey } from "../utils/index";
+
+export const renderKeyBox = props => {
+  const { handleKeyRadioChange } = props;
+  const intKeyRange = Array.from({ length: 12 }, (x, i) => i); // range from 0-11 for keys
+
+  const radioBoxes = intKeyRange.map(key => {
+    return (
+      <div key={key}>
+        <input
+          type="radio"
+          name="key"
+          id={`keyChoice${key}`}
+          value={key}
+          onChange={handleKeyRadioChange}
+        />
+        <label htmlFor="keyChoice1">{parseTrackKey(key)}</label>
+      </div>
+    );
+  });
+
+  return (
+    <div className="tab-dropdown-container">
+      <label className="tab-dropdown-heading">Key</label>
+      <form className="radio">
+        <div className="key-grid">{radioBoxes}</div>
+      </form>
+    </div>
+  );
+};
+
+export const renderModeBox = props => {
+  const { handleModalityRadioChange } = props;
+
+  return (
+    <div className="tab-dropdown-container">
+      <label className="tab-dropdown-heading">Modality</label>
+      <form className="radio">
+        <div>
+          <div>
+            <input
+              type="radio"
+              name="modality"
+              id="modeChoice1"
+              value="0"
+              onChange={handleModalityRadioChange}
+            />
+            <label htmlFor="modeChoice1">Minor</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="modality"
+              id="modeChoice2"
+              value="1"
+              onChange={handleModalityRadioChange}
+            />
+            <label htmlFor="modeChoice2">Major</label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export const renderMinMaxBox = (
+  ref,
+  heading,
+  minValue,
+  maxValue,
+  filterNameString,
+  step,
+  formatLabel
+) => {
+  const { handleApplyFilterMinMax } = ref.props;
+  return (
+    <div className="tab-dropdown-container">
+      <label className="tab-dropdown-heading">{heading}</label>
+      <div className="tab-input-container">
+        <InputRange
+          minValue={minValue}
+          maxValue={maxValue}
+          value={ref.state[filterNameString]}
+          onChange={theValue => {
+            ref.setState({ [filterNameString]: theValue });
+          }}
+          step={step}
+          formatLabel={value => `${value}${formatLabel}`}
+        />
+      </div>
+      <div className="tab-dropdown-button-container">
+        <button
+          className="button-add"
+          onClick={() =>
+            handleApplyFilterMinMax(
+              filterNameString,
+              ref.state[filterNameString].min,
+              ref.state[filterNameString].max
+            )
+          }
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
